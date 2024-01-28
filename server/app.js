3078ea6c01bf9d20b7d13086903d370e4ad29e57
Cameron McEachin <cmceachin5888@gmail.com>
2024-01-28 18:51:47 -0500

    minor updates

--- server/app.js
+++ server/app.js
@@ -1,12 +1,3 @@
-/**
- * Project Name: SocialEcho
- * Description: A social networking platform with automated content moderation and context-based authentication system.
- *
- * Author: Neaz Mahmud
- * Email: neaz6160@gmail.com
- * Date: 19th June 2023
- */
-
 require("dotenv").config();
 const express = require("express");
 const adminRoutes = require("./routes/admin.route");
